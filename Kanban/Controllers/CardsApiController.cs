4c77c9ff270c4428c0c82eb3262b7d5bc9f8cd5f
Paszel <pawel.artur.klima@gmail.com>
2017-03-17 08:55:24 +0100

    add cards

---
+++ Kanban/Controllers/CardsApiController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Kanban.Models;
+
+namespace Kanban.Controllers
+{
+    public class CardsApiController : ApiController
+    {
+        private ModelDb db = new ModelDb();
+
+        // GET: api/CardsApi
+        public IQueryable<Card> GetCard()
+        {
+            return db.Card;
+        }
+
+        // GET: api/CardsApi/5
+        [ResponseType(typeof(Card))]
+        public async Task<IHttpActionResult> GetCard(int id)
+        {
+            Card card = await db.Card.FindAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(card);
+        }
+
+        // PUT: api/CardsApi/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCard(int id, Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != card.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(card).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CardExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CardsApi
+        [ResponseType(typeof(Card))]
+        public async Task<IHttpActionResult> PostCard(Card card)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Card.Add(card);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = card.Id }, card);
+        }
+
+        // DELETE: api/CardsApi/5
+        [ResponseType(typeof(Card))]
+        public async Task<IHttpActionResult> DeleteCard(int id)
+        {
+            Card card = await db.Card.FindAsync(id);
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            db.Card.Remove(card);
+            await db.SaveChangesAsync();
+
+            return Ok(card);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CardExists(int id)
+        {
+            return db.Card.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Kanban/Controllers/CardsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Kanban.Models;
+
+namespace Kanban.Controllers
+{
+    public class CardsController : Controller
+    {
+        private ModelDb db = new ModelDb();
+
+        // GET: Cards
+        public async Task<ActionResult> Index()
+        {
+            var card = db.Card.Include(c => c.TaskSet);
+            return View(await card.ToListAsync());
+        }
+
+        // GET: Cards/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = await db.Card.FindAsync(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // GET: Cards/Create
+        public ActionResult Create()
+        {
+            ViewBag.TaskSetId = new SelectList(db.TaskSet, "Id", "Name");
+            return View();
+        }
+
+        // POST: Cards/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Description,Priority,BeginDate,EndDate,TaskSetId")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Card.Add(card);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TaskSetId = new SelectList(db.TaskSet, "Id", "Name", card.TaskSetId);
+            return View(card);
+        }
+
+        // GET: Cards/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = await db.Card.FindAsync(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TaskSetId = new SelectList(db.TaskSet, "Id", "Name", card.TaskSetId);
+            return View(card);
+        }
+
+        // POST: Cards/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Description,Priority,BeginDate,EndDate,TaskSetId")] Card card)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(card).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TaskSetId = new SelectList(db.TaskSet, "Id", "Name", card.TaskSetId);
+            return View(card);
+        }
+
+        // GET: Cards/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Card card = await db.Card.FindAsync(id);
+            if (card == null)
+            {
+                return HttpNotFound();
+            }
+            return View(card);
+        }
+
+        // POST: Cards/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Card card = await db.Card.FindAsync(id);
+            db.Card.Remove(card);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
